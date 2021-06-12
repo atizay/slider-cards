d11a47f5df13ace6f1eb79bcbf9a47eadcb7f6f7
Arthur <osartie@gmail.com>
2021-06-12 11:07:38 +0500

    Версия слайдера 1.1.0 - Функционал слайдера сделан по принципу работы плагинов (теперь можно задавать начальный активный слайд)

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  const slides = document.querySelectorAll('.slide');
+  function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide');
 
-  for (const slide of slides) {
-    slide.addEventListener('click', () => {
+    slides[activeSlide].classList.add('active');
 
-      clearActiveClasses();
-      slide.classList.add('active');
-    });
+    for (const slide of slides) {
+      slide.addEventListener('click', () => {
+
+        clearActiveClasses();
+        slide.classList.add('active');
+      });
+    }
+
+    function clearActiveClasses() {
+      slides.forEach((slide) => {
+        slide.classList.remove('active');
+      })
+    }
   }
 
-  function clearActiveClasses() {
-    slides.forEach((slide) => {
-      slide.classList.remove('active');
-    })
-  }
-  
+  slidesPlugin(4);
+
 });
